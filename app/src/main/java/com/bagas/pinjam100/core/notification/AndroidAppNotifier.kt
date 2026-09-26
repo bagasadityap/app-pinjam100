c7f6490d2904d25f8541ae57878cd1168de84a98
bagasaditya <bagasadit78@gmail.com>
2026-09-26 17:38:38 +0700

    fix: app icon

--- app/src/main/java/com/bagas/pinjam100/core/notification/AndroidAppNotifier.kt
+++ app/src/main/java/com/bagas/pinjam100/core/notification/AndroidAppNotifier.kt
@@ -48,7 +48,7 @@ class AndroidAppNotifier(
         if (!canPost()) return id
 
         val built = NotificationCompat.Builder(context, notification.channel.channelId)
-            .setSmallIcon(R.drawable.small_icon)
+            .setSmallIcon(R.mipmap.ic_launcher_monochrome)
             .setContentTitle(notification.title)
             .setContentText(notification.body)
             .setStyle(NotificationCompat.BigTextStyle().bigText(notification.body))
